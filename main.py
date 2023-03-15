b62f5c61bacf7c175f0ede9b1aaa8c1ef61ad3cd
nauqs <aquindsa10@alumnes.ub.edu>
2023-03-15 17:21:38 +0000

    Add dataset and dataloaders

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Cats and dogs")
+import matplotlib.pyplot as plt
+from utils.datasets import get_train_valid_data, get_test_data
+
+
+# Set hyperparameters
+root_dir = "data"
+batch_size = 32
+image_size = 224
+labelled_fraction = .5
+valid_fraction = .2
+
+# Dummy tests
+labelled_train_loader, unlabelled_train_loader, valid_loader = get_train_valid_data(root_dir, batch_size, image_size, labelled_fraction, valid_fraction)
+test_loader = get_test_data(root_dir, image_size, batch_size)
+
+images, masks = next(iter(labelled_train_loader))
+
+# Plot 10 pairs of images and masks in subplots
+fig, ax = plt.subplots(2, 10, figsize=(10, 2))
+for i in range(10):
+    ax[0, i].imshow(images[i].permute(1, 2, 0))
+    ax[0, i].axis("off")
+    ax[1, i].imshow(masks[i].squeeze(), cmap="gray")
+    ax[1, i].axis("off")
+plt.show()
---
+++ utils/datasets.py
@@ -0,0 +1,92 @@
+import os
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import pandas as pd
+
+
+class ImageSegmentationDataset(Dataset):
+    def __init__(self, filenames, image_size, labelled, root_dir="data"):
+        self.image_dir = os.path.join(root_dir, "images")
+        self.mask_dir = os.path.join(root_dir, "annotations/trimaps")
+        self.filenames = filenames
+        self.image_size = image_size
+        self.labelled = labelled
+        self.transforms =  transforms.Compose([transforms.ToTensor(),
+                                               transforms.Resize((image_size, image_size)),])
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, index):
+
+        # Load image and mask
+        image_path = os.path.join(self.image_dir, self.filenames[index]+".jpg")
+        image = self.transforms(Image.open(image_path))
+
+        # If unlabelled, return image only
+        if not self.labelled:
+            return image
+
+        # Else, load mask as well and return image and mask
+        mask_path = os.path.join(self.mask_dir, self.filenames[index]+".png")
+        mask = self.transforms(Image.open(mask_path))
+
+        return image, mask
+
+
+def get_train_valid_data(root_dir, batch_size, image_size, labelled_fraction=.5, valid_fraction=.2):
+    """
+    Returns a DataLoader for the labelled-training,  unlabelled-training, and validation datasets.
+    """
+
+    print("Loading training and validation data...")
+
+    columns = ["filename", "class_id", "species_id", "breed_id"]
+    
+    # Load dataset metadata  
+    train_val_df = pd.read_csv(os.path.join(root_dir, "annotations/trainval.txt"), sep=" ", header=None, names=columns)
+    train_val_filenames = train_val_df['filename'].values.tolist()
+    
+    # TODO: improve split strategy (use breed_id/class_id/species_id idk)
+    # Split in train and validation 80/20
+    shuffled_indices = torch.randperm(len(train_val_filenames))
+    train_val_filenames = [train_val_filenames[i] for i in shuffled_indices]
+    train_filenames = train_val_filenames[:int((1-valid_fraction) * len(train_val_filenames))]
+    valid_filenames = train_val_filenames[int((1-valid_fraction) * len(train_val_filenames)):]
+
+    # Split train in labelled and unlabelled
+    train_labelled_filenames = train_filenames[:int(labelled_fraction * len(train_filenames))]
+    train_unlabelled_filenames = train_filenames[int(labelled_fraction * len(train_filenames)):]
+
+    # Create training and validation datasets
+    train_dataset = ImageSegmentationDataset(train_labelled_filenames, image_size, labelled=True)
+    unlabelled_dataset = ImageSegmentationDataset(train_unlabelled_filenames, image_size, labelled=False)
+    valid_dataset = ImageSegmentationDataset(valid_filenames, image_size, labelled=True)
+
+    # Create data loaders for each dataset
+    labelled_train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    unlabelled_train_loader = DataLoader(unlabelled_dataset, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size)
+
+    return labelled_train_loader, unlabelled_train_loader, valid_loader
+
+def get_test_data(root_dir, batch_size, image_size):
+    """
+    Returns a DataLoader for the test dataset.
+    """
+
+    columns = ["filename", "class_id", "species_id", "breed_id"]
+    
+    # Load dataset metadata  
+    test_df = pd.read_csv(os.path.join(root_dir, "annotations/test.txt"), sep=" ", header=None, names=columns)
+    test_filenames = test_df['filename'].values.tolist()
+
+    # Create test dataset
+    test_dataset = ImageSegmentationDataset(test_filenames, image_size, labelled=False)
+
+    # Create data loader for test dataset
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    return test_loader
