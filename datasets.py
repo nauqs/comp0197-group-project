4ee374243b4d7ec4d70bcfd8242ad4e0981e55eb
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2023-03-17 19:59:57 +0000

    resize all images to same shape

--- datasets.py
+++ datasets.py
@@ -1,10 +1,12 @@
 import numpy as np
 import torch
 import torchvision
+import torchvision.transforms.functional as TF
 from torchvision.datasets import OxfordIIITPet
+from torch.utils.data import DataLoader
 
 
-def load_dataset(root, split):
+def load_dataset(root, split, size=224):
     """
     returns a Dataset with:
     - images as normalized float32 tensors
@@ -14,17 +16,29 @@ def load_dataset(root, split):
     - 1: foreground
     - 2: background
     - 3: not classified
+    
+    Both images and labels are resized to [size, size].
     """
     image_transform = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        torchvision.transforms.Resize([size, size]),
     ])
-    label_transform = lambda label: torch.from_numpy(np.array(label))
+    label_transform = torchvision.transforms.Compose([
+        lambda label: torch.from_numpy(np.array(label)),
+        lambda label: TF.resize(label[None], [size, size])[0],
+    ])
     ds = OxfordIIITPet(root=root, split=split, target_types='segmentation', download=True, transform=image_transform, target_transform=label_transform)
     return ds
 
 
 def create_datasets(root='/tmp/adl_data', valid_frac=0.2, labelled_frac=0.125):
+    """
+    Returns:
+    - train_all_ds: labeled and unlabeled training images
+    - train_lab_ds: labeled training images
+    - valid_ds: validation images
+    """
     trainval_ds = load_dataset(root, 'trainval')
     test_ds = load_dataset(root, 'test')
 
