af79b6a01531ccdb7a7462ae4c1e3b91c386e685
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2023-03-18 09:44:02 +0000

    add model evaluation function

--- train.py
+++ train.py
@@ -3,6 +3,35 @@ import torch.nn.functional as F
 from time import time
 
 
+@torch.no_grad()
+def eval(model, dataloader):
+    """
+    Evaluates model loss and accuracy over one epoch.
+    """
+    device = next(model.parameters()).device
+    epoch_loss = 0
+    epoch_acc = 0
+
+    # iterate over batches
+    n_batches = len(dataloader)
+    for images, labels in dataloader:
+        images, labels = images.to(device), labels.to(device)
+
+        # get a mask of labeled pixels (foreground/background)
+        labeled_pixels = labels != 3
+
+        # get model predictions
+        logits = model(images)['out'][:, 0]
+
+        # compute loss, accuracy on labeled pixels
+        loss = F.binary_cross_entropy_with_logits(logits[labeled_pixels], labels[labeled_pixels].float())
+        acc = ((logits[labeled_pixels] > 0.5) == labels[labeled_pixels]).float().mean()
+        epoch_loss += loss.item() / n_batches
+        epoch_acc += acc.item() / n_batches
+    
+    return epoch_loss, epoch_acc
+
+
 def train(model, train_all_dl, train_lab_dl, valid_dl, epochs=1, lr=1e-3):
     """
     Trains a segmentation model.
@@ -53,7 +82,8 @@ def train(model, train_all_dl, train_lab_dl, valid_dl, epochs=1, lr=1e-3):
             print(loss.item())
 
         # print statistics
+        val_loss, val_acc = eval(model, valid_dl)
         epoch_time = -t + (t := time()) # time per epoch
-        print(f'epoch={epoch+1:2}, time={epoch_time:5.2f}, {train_acc=:4.2%}, {train_loss=:.5f}')
+        print(f'epoch={epoch+1:2}, time={epoch_time:5.2f}, {train_acc=:4.2%}, {valid_acc=:4.2%}')
 
     return model
