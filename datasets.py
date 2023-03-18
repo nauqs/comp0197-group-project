fix image normalization stats

datasets.py
@@ -8,6 +8,13 @@ from torchvision.datasets import OxfordIIITPet
 from torch.utils.data import DataLoader
 
 
+# normalizing constants (mean, std) for datasets
+# https://github.com/pytorch/vision/blob/b403bfc771e0caf31efd06d43860b09004f4ac61/torchvision/transforms/_presets.py#LL44C35-L44C56
+DS_STATS = {
+    'classification': ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+}
+
+
 class OxfordIIITPetCached(OxfordIIITPet):
 
     @cache
@@ -27,13 +34,17 @@ def load_dataset(root, split, size=224):
     - 2: not classified
 
     Both images and labels are resized to [size, size].
+
+    Note: we are using a pretrained ResNet with ImageNet weights as the backbone.
+    The backbone assumes that input images are normalized to a specific
+    mean and std (defined above as 'DS_STATS').
     """
 
     # define image and label transforms
     image_transform = torchvision.transforms.Compose(
         [
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            torchvision.transforms.Normalize(*DS_STATS['classification']),
             torchvision.transforms.Resize([size, size]),
         ]
     )

utils.py
@@ -1,12 +1,19 @@
 import torch
 import torchvision
 from pathlib import Path
+import datasets
+
+
+def unnormalize_images(images):
+    mean, std = torch.tensor(datasets.DS_STATS['classification'])
+    images = images * std[None, :, None, None] + mean[None, :, None, None]
+    return images.clip(0, 1)
 
 
 def visualize_predictions(images, logits, filename):
 
     # normalize images to [0, 1]
-    images = images * 0.5 + 0.5
+    images = unnormalize_images(images)
 
     # convert logits to class probabilites
     probs = 1 - torch.sigmoid(logits)