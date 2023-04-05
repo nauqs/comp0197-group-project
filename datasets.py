372330d43e32b2427de62ddaa2d07c764db48aa1
Dominik Schmidt <schmidtdominik30@gmail.com>
2023-04-05 21:11:10 +0200

    updated datasets

--- datasets.py
+++ datasets.py
@@ -88,27 +88,19 @@ def create_datasets(root="/tmp/adl_data", valid_frac=0.2, labelled_frac=0.0625):
         train_all_ds, (1 - labelled_frac, labelled_frac), generator=rng
     )
 
-    return train_all_ds, train_lab_ds, train_unlab_ds, valid_ds, test_ds
+    return train_all_ds, train_lab_ds, valid_ds, test_ds
 
 
-def create_dataloaders(batch_size=8, image_size=224,affine_transform = False, *args, **kwargs):
+def create_dataloaders(batch_size=8, image_size=224, affine_transform = False, *args, **kwargs):
     """
     Generates datasets using 'create_datasets' and crates a
     DataLoader for each dataset.
     """
 
     datasets = create_datasets(*args, **kwargs)
-    if affine_transform == True:
-
-        dataloaders = [
-            DataLoader(ds, batch_size=batch_size, shuffle=True) for ds in datasets
-        ]
-
-    else:
-
-        dataloaders = [
-            DataLoader(ds, batch_size=batch_size, shuffle=True) for ds in datasets
-        ]
+    dataloaders = [
+        DataLoader(ds, batch_size=batch_size, shuffle=True) for ds in datasets
+    ]
 
     return dataloaders
 
